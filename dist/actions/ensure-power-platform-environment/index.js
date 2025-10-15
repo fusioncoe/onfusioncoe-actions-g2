984edc894d3148484309dccb3a9884e1b1ed6200
Ryan Millett <ryan.millett@74bravo.com>
2025-10-15 18:05:28 -0400

    ensure-power-platform-environment step 1 & 2 of 3.

--- dist/actions/ensure-power-platform-environment/index.js
+++ dist/actions/ensure-power-platform-environment/index.js
@@ -42166,10 +42166,62 @@ async function DecryptData(encryptedData, pemKey) {
 async function executeAction(args) {
   import_core3.default.info("currently running ensure-power-platform-environment");
   const fsnxClient = new FsnxApiClient(args);
-  await fsnxClient.OnStep("get-environment-details", async () => {
-    const response = await fsnxClient.ExecuteHttpAction("get-environment");
-    const output = { ...response.body };
-    fsnxClient.SubmitOutput(output);
+  await fsnxClient.OnStep("get-dataverse-details", async () => {
+    const output = {};
+    const orgResponse = await fsnxClient.ExecuteHttpAction("get-organization-id");
+    if (orgResponse.ok) {
+      import_core3.default.info(`Found Dataverse Organization with ID: ${orgResponse.body.value[0].organizationid}`);
+      output.Organization = orgResponse.body;
+    } else {
+      output.error = {
+        code: orgResponse.status,
+        message: orgResponse.body.error.message
+      };
+      await fsnxClient.SubmitOutput(output);
+      import_core3.default.error(orgResponse.body.error.message);
+      throw new Error(`Failed to retrieve Dataverse Organization ID: ${orgResponse.status} : ${orgResponse.body.error.message}`);
+    }
+    const rootBuResponse = await fsnxClient.ExecuteHttpAction("get-root-business-unit");
+    if (rootBuResponse.ok) {
+      import_core3.default.info(`Found Dataverse Root Business Unit with ID: ${rootBuResponse.body.value[0].businessunitid}`);
+      output.RootBusinessUnit = rootBuResponse.body;
+    } else {
+      output.error = {
+        code: rootBuResponse.status,
+        message: rootBuResponse.body.error.message
+      };
+      await fsnxClient.SubmitOutput(output);
+      import_core3.default.error(rootBuResponse.body.error.message);
+      throw new Error(`Failed to retrieve Dataverse Root Business Unit ID: ${rootBuResponse.status} : ${rootBuResponse.body.error.message}`);
+    }
+    const SysAdminRoleResponse = await fsnxClient.ExecuteHttpAction("get-system-administrator-roleid");
+    if (SysAdminRoleResponse.ok) {
+      import_core3.default.info(`Found Dataverse System Administrator Role with ID: ${SysAdminRoleResponse.body.value[0].roleid}`);
+      output.SystemAdminRole = SysAdminRoleResponse.body;
+    } else {
+      output.error = {
+        code: SysAdminRoleResponse.status,
+        message: SysAdminRoleResponse.body.error.message
+      };
+      await fsnxClient.SubmitOutput(output);
+      import_core3.default.error(SysAdminRoleResponse.body.error.message);
+      throw new Error(`Failed to retrieve Dataverse System Administrator Role ID: ${SysAdminRoleResponse.status} : ${SysAdminRoleResponse.body.error.message}`);
+    }
+    const SysCustomRoleResponse = await fsnxClient.ExecuteHttpAction("get-system-customizer-roleid");
+    if (SysCustomRoleResponse.ok) {
+      import_core3.default.info(`Found Dataverse System Customizer Role with ID: ${SysCustomRoleResponse.body.value[0].roleid}`);
+      output.SystemCustomizerRole = SysCustomRoleResponse.body;
+    } else {
+      output.error = {
+        code: SysCustomRoleResponse.status,
+        message: SysCustomRoleResponse.body.error.message
+      };
+      await fsnxClient.SubmitOutput(output);
+      import_core3.default.error(SysCustomRoleResponse.body.error.message);
+      throw new Error(`Failed to retrieve Dataverse System Customizer Role ID: ${SysCustomRoleResponse.status} : ${SysCustomRoleResponse.body.error.message}`);
+    }
+    import_core3.default.info(JSON.stringify(output));
+    await fsnxClient.SubmitOutput(output);
   });
 }
 // Annotate the CommonJS export names for ESM import in node:
--- tests/ensure-power-platform-environment.js
+++ tests/ensure-power-platform-environment.js
@@ -52,6 +52,88 @@ const test = async () => {
 
     }); 
 
+   await fsnxClient.OnStep("get-dataverse-details", async () => {
+
+        const output = {};
+
+        // Process Actions    
+        const orgResponse = await fsnxClient.ExecuteHttpAction("get-organization-id");
+        if (orgResponse.ok)
+        {
+            core.info(`Found Dataverse Organization with ID: ${orgResponse.body.value[0].organizationid}`);
+            output.Organization = orgResponse.body;
+        }
+        else
+        {
+            output.error = {
+                code: orgResponse.status,
+                message: orgResponse.body.error.message
+            }
+            await fsnxClient.SubmitOutput (output);  
+            core.error(orgResponse.body.error.message);
+            throw new Error(`Failed to retrieve Dataverse Organization ID: ${orgResponse.status} : ${orgResponse.body.error.message}`); 
+        }
+
+        const rootBuResponse = await fsnxClient.ExecuteHttpAction("get-root-business-unit");
+        if (rootBuResponse.ok)
+        {
+            core.info(`Found Dataverse Root Business Unit with ID: ${rootBuResponse.body.value[0].businessunitid}`);
+            output.RootBusinessUnit = rootBuResponse.body;
+        }
+        else
+        {
+            output.error = {
+                code: rootBuResponse.status,
+                message: rootBuResponse.body.error.message
+            }   
+            await fsnxClient.SubmitOutput (output);  
+            core.error(rootBuResponse.body.error.message);
+            throw new Error(`Failed to retrieve Dataverse Root Business Unit ID: ${rootBuResponse.status} : ${rootBuResponse.body.error.message}`); 
+        }
+
+
+        const SysAdminRoleResponse = await fsnxClient.ExecuteHttpAction("get-system-administrator-roleid");
+        if (SysAdminRoleResponse.ok)
+        {
+            core.info(`Found Dataverse System Administrator Role with ID: ${SysAdminRoleResponse.body.value[0].roleid}`);
+            output.SystemAdminRole = SysAdminRoleResponse.body;
+        }
+        else
+        {
+            output.error = {
+                code: SysAdminRoleResponse.status,
+                message: SysAdminRoleResponse.body.error.message
+            }
+            await fsnxClient.SubmitOutput (output);  
+            core.error(SysAdminRoleResponse.body.error.message);
+            throw new Error(`Failed to retrieve Dataverse System Administrator Role ID: ${SysAdminRoleResponse.status} : ${SysAdminRoleResponse.body.error.message}`); 
+        }
+
+        const SysCustomRoleResponse = await fsnxClient.ExecuteHttpAction("get-system-customizer-roleid");
+        if (SysCustomRoleResponse.ok)
+        {
+            core.info(`Found Dataverse System Customizer Role with ID: ${SysCustomRoleResponse.body.value[0].roleid}`);
+            output.SystemCustomizerRole = SysCustomRoleResponse.body;
+        }
+        else
+        {
+            output.error = {
+                code: SysCustomRoleResponse.status,
+                message: SysCustomRoleResponse.body.error.message
+            }
+            await fsnxClient.SubmitOutput (output);  
+            core.error(SysCustomRoleResponse.body.error.message);
+            throw new Error(`Failed to retrieve Dataverse System Customizer Role ID: ${SysCustomRoleResponse.status} : ${SysCustomRoleResponse.body.error.message}`); 
+        }
+
+        core.info(JSON.stringify(output));
+
+        await fsnxClient.SubmitOutput (output)
+
+    }); 
+
+
+
 };
 
 test();
